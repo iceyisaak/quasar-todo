305c0db799b2201c507bb4b63db8e9c74f459469
Icey Isaak <iceyisaak@gmail.com>
2020-01-06 10:06:40 +0100

    first commit

---
+++ src/pages/Help.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus quam sequi, repellendus atque provident suscipit excepturi beatae delectus ut rem mollitia a dolorem, dolore eum totam maxime dolorum similique maiores!</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores hic officia alias voluptate cumque nesciunt dolorum nisi autem omnis! Doloribus alias atque voluptatem debitis dignissimos, aut numquam sed quibusdam error.</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
